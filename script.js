4d7e77cab4a6d2bbcd32f8ab3e2e506d2797ff1f
Lesley_Heyer <pingpongkat@yahoo.com>
2021-05-25 22:06:25 -0500

    added image to image folder, created dynamic code with script.js file, and edited index.html and style.css

---
+++ script.js
@@ -0,0 +1,37 @@
+function init () {
+    const noButton = document.getElementById("chooseNo");
+    const button = document.getElementById("chooseYes");
+    const paragraph = document.getElementById("husbandChoice");
+
+    function husbandDecision() {
+        document.getElementById("husbandChoice").innerHTML = 'He said "Yes" too!!';
+          }
+         button.addEventListener("click", husbandDecision);
+
+    function yesButton() {
+        button.style.background = "green";}
+        button.addEventListener("mouseover", yesButton);
+
+    function yesButtonDefault() {
+        button.style.background = "";}
+        button.addEventListener("mouseout", yesButtonDefault);
+  
+    function chooseNoButton() {
+        noButton.style.backgroundColor = "red";}
+        noButton.addEventListener("mouseover", chooseNoButton);
+
+    function chooseNoDefault() {
+        noButton.style.backgroundColor = "";}
+        noButton.addEventListener("mouseout", chooseNoDefault);
+
+    function confirmMarriage() {
+        let response = window.confirm("Are you sure you don't want to marry him?");
+        if (response) {
+            paragraph.innerHTML = "No hard feelings! (Silently judging you)"
+            }
+          } 
+        noButton.addEventListener("click", confirmMarriage);
+
+}
+
+window.addEventListener("load", init);
